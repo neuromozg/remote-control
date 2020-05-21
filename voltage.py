e5344ce0c463f56d652196891fc5eb8f1c385336
ArtemZaZ <temka.911@mail.ru>
2020-05-21 13:30:53 +0300

    replace lib in voltage file

--- voltage.py
+++ voltage.py
@@ -1,23 +1,15 @@
+#!/usr/bin/env python
+from ina219 import INA219
+from ina219 import DeviceRangeError
 import time
-import board
-from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219
 
-i2c_bus = board.I2C()
+SHUNT_OHMS = 0.1
 
-ina219 = INA219(i2c_bus)
-
-# optional : change configuration to use 32 samples averaging for both bus voltage and shunt voltage
-ina219.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-ina219.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-# optional : change voltage range to 16V
-ina219.bus_voltage_range = BusVoltageRange.RANGE_16V
+ina = INA219(SHUNT_OHMS)
+ina.configure()
 
 # measure and display loop
 while True:
-    bus_voltage = ina219.bus_voltage        # voltage on V- (load side)
-    shunt_voltage = ina219.shunt_voltage    # voltage between V+ and V- across the shunt
-    current = ina219.current                # current in mA
-
-    print("Напряжение: %.2f" % bus_voltage)
-    print("Ток: %.3f" % (current/1000))
+    print("Напряжение: %.2f В" % ina.voltage())
+    print("Ток: %.3f А" % (ina.current()/1000))
     time.sleep(3)
